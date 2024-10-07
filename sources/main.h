92f4489f738eb46b88338669352e23ea365e42ea
Gavin McMahon <jacqi.larue@gmail.com>
2024-10-06 22:56:03 -0500

    more

--- sources/main.h
+++ sources/main.h
@@ -4,3 +4,30 @@
 #include <flecs.h>
 
 #include "playerstate.h"
+
+/*typedef struct playerwk
+{
+	//float hpos;
+	float dotp;
+	Transform t;
+	//Vector3 eff;
+	//Vector3 acc;
+	Vector3 spd;
+	Vector3 gravity;
+	Vector3 wall_normal;
+	Vector3 floor_normal;
+	Vector3 last_up;
+	unsigned int attr;
+	unsigned int last_attr;
+	bool grounded;
+	unsigned int flag;
+	float last_turn;
+	float frict_mult;
+	TimersAndSuch timer;
+	//ModelStruct mS;
+	player_parameter p;
+	player_state pl_state;
+	Capsule collider;
+	SONIC_INPUT input;
+	Camera cam;
+} playerwk;*/
---
+++ sources/playerphys.h
@@ -0,0 +1,61 @@
+#pragma once
+
+#include "main.h"
+
+typedef struct player_parameter
+{
+	int jump2_timer;
+	float pos_error;
+	float lim_h_spd;
+	float lim_v_spd;
+	float max_x_spd;
+	float max_psh_spd;
+	float jmp_y_spd;
+	float nocon_speed;
+	float slide_speed;
+	float jog_speed;
+	float run_speed;
+	float rush_speed;
+	float crash_speed;
+	float dash_speed;
+	float jmp_addit;
+	float run_accel;
+	float air_accel;
+	float slow_down;
+	float run_break;
+	float air_break;
+	float air_resist_air;
+	float air_resist;
+	float air_resist_y;
+	float air_resist_z;
+	float grd_frict;
+	float grd_frict_z;
+	float lim_frict;
+	float rat_bound;
+	float rad;
+	float height;
+	float weight;
+	float eyes_height;
+	float center_height;
+} player_parameter;
+
+typedef struct spin_parameter 
+{
+	float jmp_dash_h_speed;
+	float initial_spin_speed;
+	float max_spin_speed;
+	float spin_addit;
+} spin_parameter;
+
+typedef Vector3 speed;
+
+typedef struct TimersAndSuch
+{
+	int jump_timer;
+	float spring_timer;
+	float dashpanel_timer;
+	float dashring_timer;
+	float rail_debounce;
+	float rail_trick;
+	float spindash_speed;
+} TimersAndSuch;
